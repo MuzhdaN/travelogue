63f402852f448365e4a48f187e4226fce5b8a5f4
MuzhdaN <muzhda.noorzad4@gmail.com>
2023-03-31 07:25:23 +0000

    Connected react app to api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
+import "./api/axiosDefaults";
+import SignIpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -13,7 +15,7 @@ function App() {
           <Route exact path='/write' render={() => <h1>Write</h1>} />
           <Route exact path='/saved' render={()=> <h1>Reading List</h1>} />
           <Route exact path='/signin' render={()=> <h1>Sign in</h1>} />
-          {/* <Route exact path='/signup' render={()=> <SignUpForm /> } /> */}
+          <Route exact path='/signup' render={()=> <SignIpForm /> } />
           <Route render={() => <p>Page not found</p>} />
         </Switch>
       </Container>
