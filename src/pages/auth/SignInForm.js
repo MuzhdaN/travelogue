dafcdaf687efab7660b908e49eeee3e43507dc2e
MuzhdaN <muzhda.noorzad4@gmail.com>
2023-04-02 06:56:12 +0000

    Added signin page

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 
 import { Container, Card, Row, Col, Image, Form, Button, Alert } from 'react-bootstrap';
@@ -8,9 +8,12 @@ import styles from '../../styles/SignInUpForm.module.css';
 import btnStyles from '../../styles/Button.module.css';
 import logo from '../../assets/logo.png';
 import signUphero from '../../assets/signUphero.jpg';
+import { SetCurrentUserContext } from '../../App';
 
 
 function SignInForm() {
+  const setCurrentUser = useContext(SetCurrentUserContext);
+
   const [signInData, setSignInData] = useState({
     username: '',
     password: '',
@@ -30,7 +33,8 @@ function SignInForm() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("/dj-rest-auth/login/", signInData);
+      const {data} = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
       history.push("/");
     } catch (err) {
       setErrors(err.response?.data);
