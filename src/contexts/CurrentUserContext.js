90cf39806f939477732f318c38e2fe722af16f1d
MuzhdaN <muzhda.noorzad4@gmail.com>
2023-04-04 08:18:52 +0000

    removed extra import

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -2,7 +2,6 @@ import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useHistory } from "react-router-dom";
-import { response } from "msw";
 
 
 export const CurrentUserContext = createContext();
