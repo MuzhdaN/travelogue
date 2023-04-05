281e84aaff89a2ca6a22d3226be054e84958a597
MuzhdaN <muzhda.noorzad4@gmail.com>
2023-04-05 09:27:15 +0000

    Added Navbar toggle functionailty

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,14 @@ import { NavLink } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from 'axios';
+import clickOutsideToggle from '../hooks/useClickOutisdeToggle';
 
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const {expanded, setExpanded, ref} = clickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -81,13 +84,17 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={navStyles.NavBar} expand="md" fixed='top'>
+    <Navbar expanded={expanded} className={navStyles.NavBar} expand="md" fixed='top'>
       <Container>
         <Navbar.Brand>
             <img src={logo} alt="logo" height="50" className="me-2" />
             <span className="mt-1">Travelogue</span>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle 
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+         />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
 
